93f7f780db7363acba67c4ee93cbd3063fba1be9
OwenFink <owen.fink@uconn.edu>
2016-10-26 14:37:57 -0400

    Add files via upload

--- Pokemon.java
+++ Pokemon.java
@@ -1,6 +1,10 @@
 package pokemon;
 
+
+
 public abstract class Pokemon {
+	public String name;
+	public String description;
 	public int ID, catchRate, fleeChance;
 	public int baseMaxHP, baseAttack, baseDefense, baseSpecialAttack, baseSpecialDefense, baseSpeed;
 	public int IVmaxHP, IVattack, IVdefense, IVspecialAttack, IVspecialDefense, IVspeed;
@@ -8,15 +12,18 @@ public abstract class Pokemon {
 	public int level, currentHP, maxHP, attack, defense, specialAttack, specialDefense, speed;
 	public enum Type {NORMAL, FIRE, WATER, ELECTRIC, GRASS, ICE, FIGHTING, POISON,
 		GROUND, FLYING, PSYCHIC, BUG, ROCK, GHOST, DRAGON, DARK, STEEL, FAIRY};
+	public enum Gender{Male, Female};
 	public Type t;
-	public String description;
+	public Gender g;
+	public Status status;
+	public Item item;
 	/*Stat = floor((2 * B + I + E) * L / 100 + L + 10)
 	 * B = base stat
 	 * I = IV
 	 * E = Effort Value (NOT IMPLEMENTED)
 	 * L = Level
 	 */
-	public void calculateStats(Pokemon p) {
+	public Pokemon calculateStats(Pokemon p) {
 		p.maxHP				=(int) Math.floor(((2*p.baseMaxHP+p.IVmaxHP)*p.level)/(110+p.level));
 		p.currentHP			= maxHP;
 		p.attack			=(int) Math.floor(((2*p.baseAttack+p.IVattack)*p.level)/(110+p.level));
@@ -24,10 +31,15 @@ public abstract class Pokemon {
 		p.specialAttack		=(int) Math.floor(((2*p.baseSpecialAttack+p.IVspecialAttack)*p.level)/(110+p.level));
 		p.specialDefense	=(int) Math.floor(((2*p.baseSpecialDefense+p.IVspecialDefense)*p.level)/(110+p.level));
 		p.speed				=(int) Math.floor(((2*p.baseSpeed+p.IVspeed)*p.level)/(110+p.level));
+		return p;
 	}
-	public void levelUp(Pokemon p) {
+	public Pokemon levelUp(Pokemon p) {
 		p.level += 1;
-		calculateStats(p);
+		p = calculateStats(p);
+		return p;
 	}
-	
+	public Pokemon giveItem(Item i){
+		item = i;
+		return this;
+	}
 }
